ab935c311d32e6bbfed3b6dc0dea5232678c82e6
Wren Hsieh <wrenhsieh18@gmail.com>
2021-11-20 17:25:43 -0600

    finished the studio

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 // TODO: add code here
+const displayArrayWithSpace = (arr) => {
+    if (arr.length === 1) {
+        return arr[0];
+    } else {
+        return arr.shift() + ", " + displayArrayWithSpace(arr);
+    }
+}
+
+const outputHTML = (obj) => {
+    if (obj.active === true) {
+        return `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${obj.firstName} ${obj.lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${obj.hoursInSpace}</li>
+                    <li style = "color: green;">Active: ${obj.active}</li>
+                    <li>Skills: ${displayArrayWithSpace(obj.skills)}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${obj.picture}">
+        </div>`
+    }
+    return `
+    <div class="astronaut">
+        <div class="bio">
+            <h3>${obj.firstName} ${obj.lastName}</h3>
+            <ul>
+                <li>Hours in space: ${obj.hoursInSpace}</li>
+                <li>Active: ${obj.active}</li>
+                <li>Skills: ${displayArrayWithSpace(obj.skills)}</li>
+            </ul>
+        </div>
+        <img class="avatar" src="${obj.picture}">
+    </div>`
+}
+
+const init = () => {
+    const allCrewDisplay = document.getElementById("container");
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( (response) => {
+        console.log(response);
+        response.json().then( (json) => {
+            console.log(json);
+            let allHoursInSpace = [];
+            for (let astronaut of json) {
+                allHoursInSpace.push(astronaut.hoursInSpace);
+            }
+            allHoursInSpaceSorted = allHoursInSpace.sort((a, b) => {return b - a})
+            for (let hours of allHoursInSpaceSorted) {
+                for (let astronaut of json) {
+                    if (hours === astronaut.hoursInSpace) {
+                        allCrewDisplay.innerHTML += outputHTML(astronaut)
+                    }
+                }
+            }
+            const countOfAstronauts = document.getElementById("count");
+            countOfAstronauts.innerHTML = `Total Count of Astronauts: ${json.length}`
+        })
+    })
+}
+
+window.addEventListener("load", init)
